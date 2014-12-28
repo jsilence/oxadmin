9b156f500f79fc47ec1ae0b6c360997b98f10578
Dennis Wilson <dw@risu.io>
2014-12-28 12:19:56 +0100

    feat(upload-done): modal view for qr codes

--- dev/public/scripts/thinbin/upload/src/controllers/uploadDoneController.js
+++ dev/public/scripts/thinbin/upload/src/controllers/uploadDoneController.js
@@ -3,11 +3,23 @@ angular.module('io.risu.thinbin.upload')
     ['$scope', '$routeParams', '$location', 'UploadService', 'FileService',
         function ($scope, $routeParams, $location, UploadService, FileService) {
 
+            $scope.qrCodeType = 'Thinbin Url';
+            $scope.qrCodeData = true;
+
             FileService
                 .readPlaintextFileById($routeParams.id)
                 .then(function (file) {
                     $scope.viewUrl = file.viewFileUrl;
                     $scope.downloadUrl = file.downloadFileUrl;
                 });
+
+            $scope.showQrCode = function showQrCode(type, data) {
+                $scope.qrCodeType = type;
+                $scope.qrCodeData = data;
+            };
+
+            $scope.hideQrCode = function hideQrCode() {
+                $scope.qrCodeData = undefined;
+            };
         }
     ]);
